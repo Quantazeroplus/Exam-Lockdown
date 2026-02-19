23d9c914d4da03906d1ca98f2146f8b0408daf90
sandy <sandeep620670@gmail.com>
2026-02-19 14:54:33 +0530

    new version

--- SETUP_LAB.bat
+++ SETUP_LAB.bat
@@ -1,21 +1,32 @@
 @echo off
-title GEC WC Exam Installer 
+title GEC WC Exam Installer 
 color 0B
+
+:: Define Installer Colors (ANSI)
+set "ESC="
+set "GRN=%ESC%[92m"
+set "WHT=%ESC%[97m"
+set "ORN=%ESC%[33m"
+set "RESET=%ESC%[0m"
 
 echo ======================================================
 echo    GEC WEST CHAMPARAN - ( Powered by Quantazeroplus )
 echo ======================================================
 echo.
-echo [1/2] Creating  GEC_Exam.bat...
+echo [1/2] Creating GEC_Exam.bat...
 
-:: REFERENCE LINE CHANGE: Universal Desktop Detection
+:: Universal Desktop Detection
 for /f "usebackq tokens=2,*" %%A in (`reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Desktop`) do set "MY_DESKTOP=%%B"
 call set "TARGET=%MY_DESKTOP%\GEC_Exam.bat"
 
 echo @echo off > "%TARGET%"
-echo setlocal >> "%TARGET%"
-echo set "SECRET=Gecwc1234@" >> "%TARGET%"
-echo set "LOGFILE=%TEMP%\exam_logs.txt" >> "%TARGET%"
+echo setlocal enabledelayedexpansion >> "%TARGET%"
+echo set "SECRET=YOUR PASSWORD" >> "%TARGET%"
+:: Define Colors for Student Script
+echo set "RED=%ESC%[91m" >> "%TARGET%"
+echo set "ORN=%ESC%[33m" >> "%TARGET%"
+echo set "WHT=%ESC%[97m" >> "%TARGET%"
+echo set "RESET=%ESC%[0m" >> "%TARGET%"
 
 :: Kill explorer immediately
 echo taskkill /f /im explorer.exe ^>nul 2^>^&1 >> "%TARGET%"
@@ -24,29 +35,31 @@ echo :EXAM_LOOP >> "%TARGET%"
 echo mode con: cols=70 lines=20 >> "%TARGET%"
 echo cls >> "%TARGET%"
 echo echo ====================================================== >> "%TARGET%"
-echo echo   EXAM IN PROGRESS - DO NOT CLOSE THIS WINDOW >> "%TARGET%"
+echo echo !RED!  WARNING....... EXAM IN PROGRESS - DO NOT CLOSE (X) THIS WINDOW !RESET! >> "%TARGET%"
+echo echo !ORN!  ONLY MINIMIZE IS ALLOWED !RESET! >> "%TARGET%"
 echo echo ====================================================== >> "%TARGET%"
 echo echo. >> "%TARGET%"
-echo echo   1. Dont use these keys . >> "%TARGET%"
-echo echo   2. Write your answer on the paper >> "%TARGET%"
+echo echo !WHT!  1. Dont use these keys . >> "%TARGET%"
+echo echo   2. Write your answer on the paper !RESET! >> "%TARGET%"
 echo echo. >> "%TARGET%"
 
-:: Start Edge and WAIT for it to close
-echo start /wait msedge --kiosk "https://drive.google.com/uc?export=view&id=1T_XLcLb30OgGSg0usY6FTzniux19ilhL" --edge-kiosk-type=fullscreen >> "%TARGET%"
+:: Start Edge and WAIT
+echo start /wait msedge --kiosk "YOUR DRIVE LINK" --edge-kiosk-type=fullscreen >> "%TARGET%"
 
 echo :UNLOCK_PHASE >> "%TARGET%"
 echo cls >> "%TARGET%"
 echo echo ====================================================== >> "%TARGET%"
-echo echo   EXAM ENDED - ENTER CO-ORDINATER PASSWORD >> "%TARGET%"
+echo echo !RED!  WARNING: DON'T CLOSE (X) THIS WINDOW!RESET! >> "%TARGET%"
+echo echo !ORN!  MINIMIZE IS ALLOWED !RESET! >> "%TARGET%"
 echo echo ====================================================== >> "%TARGET%"
+echo echo !WHT!EXAM ENDED - ENTER CO-ORDINATER PASSWORD!RESET! >> "%TARGET%"
 echo set /p "user_pass=PASSWORD: " >> "%TARGET%"
 
-:: REFERENCE LINE CHANGE: Stable IF logic to prevent auto-exit
 echo if NOT "%%user_pass%%"=="%%SECRET%%" goto WRONG_PASS >> "%TARGET%"
 echo goto RESTORE >> "%TARGET%"
 
 echo :WRONG_PASS >> "%TARGET%"
-echo echo Wrong Password! Re-opening exam... >> "%TARGET%"
+echo echo !RED!Wrong Password! Re-opening exam...!RESET! >> "%TARGET%"
 echo timeout /t 3 >> "%TARGET%"
 echo goto EXAM_LOOP >> "%TARGET%"
 
@@ -58,5 +71,9 @@ echo del "%%~f0" ^& exit >> "%TARGET%"
 echo [2/2] Finalizing...
 timeout /t 2 >nul
 cls
-echo SUCCESS! Run GEC_Exam.bat on the student PC.
+
+:: Success Message in Green
+echo %GRN%SUCCESS! Exam file successfully installed.%WHT%
+echo You can now find GEC_Exam.bat on your Desktop.
+echo %RESET%
 pause
